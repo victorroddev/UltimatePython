be4637336952e31786c05af74ce42e8288b5c8aa
victorroddev <javier.rod.dev@gmail.com>
2024-06-13 14:09:25 -0600

    ADD 06 agregar y elminar

---
+++ tipos-avanzados/05-buscar-elemento.py
@@ -0,0 +1,7 @@
+mascotas = ["Pelusa", "Wolfgang", "Pulga", "Felipe", "Wolfgang", "Chanchito feliz"]
+
+
+print(mascotas.count("Wolfgang")) #metodo count para contar las veces que se encuentra un elemento en una listav  
+if "Wolfgang" in mascotas:
+    print(mascotas.index("Wolfgang")) #sintaxis para buscar elementos en una lista
+
